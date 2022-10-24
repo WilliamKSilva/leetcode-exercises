58d02c0fa445351d44fd225e3637629524ffba2e
William <wilz.kelvin12@gmail.com>
2022-10-24 16:52:17 -0300

    feat: exercise 1, given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target

---
+++ exercise-1.js
@@ -0,0 +1,21 @@
+
+// Like a javascript reduce
+
+var twoSum = function(nums, target) {    
+  let array = [];
+
+  for (i = 0; i < nums.length; i++) {
+    for (j = i + 1; j < nums.length; j++) {
+      let sum = nums[i] + nums[j];     
+
+      if (sum === target) {
+        array[0] = i;
+        array[1] = j;
+      }
+    }
+  }
+
+  return array;
+};
+
+console.log(twoSum([2, 5, 5, 11], 10));
