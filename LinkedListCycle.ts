d9ea9193413268df1e4ffaa5aebe92010c3e8f63
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-01-22 17:34:23 -0300

    feat: add LinkedListCycle solution: Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter. Return true if there is a cycle in the linked list. Otherwise, return false.

---
+++ LinkedListCycle.ts
@@ -0,0 +1,30 @@
+
+  class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+  }
+ 
+
+function hasCycle(head: ListNode | null): boolean {
+    const arr: ListNode[] = []
+    let curr = head as ListNode
+
+    if (!curr) {
+        return false;
+    }
+
+    while (curr.next) {
+        if (arr.includes(curr)) {
+            return true
+        };
+
+        arr.push(curr);
+        curr = curr.next;
+    } 
+
+    return false;
+};
