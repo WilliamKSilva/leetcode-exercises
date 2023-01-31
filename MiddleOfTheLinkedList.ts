2fcedbe4751dc240955b18850578bb3ebc8b9750
WilliamKSilva <wilz.kelvin12@gmail.com>
2023-01-31 17:04:53 -0300

    feat: add MiddleOfTheLinkedList solution: Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.

--- MiddleOfTheLinkedList.ts
+++ MiddleOfTheLinkedList.ts
@@ -1,14 +1,13 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
+
+
+class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+          this.next = (next===undefined ? null : next)
+      }
+  }
 
 function middleNode(head: ListNode | null): ListNode | null {
     let length = 0;
